cbb6e5f783d133b07f9f049f8b2498ba2ea652a0
Gaurav Menghani <gauravm@fb.com>
2015-06-14 14:34:47 -0700

    Tokenizing the input

--- eclisp.go
+++ eclisp.go
@@ -3,10 +3,30 @@ package main
 import (
 	// "bufio"
 	"fmt"
+	"strings"
 	// "os"
 	"github.com/tiborvass/uniline"
 );
 
+func tokenize(line string) []string {
+	tokens := strings.Split(strings.Replace(strings.Replace(line, "(", " ( ", -1), ")", " ) ", -1), " ")
+	noWSTokens := make([]string, 0)
+	for _, token := range tokens {
+		if token != "" {
+			noWSTokens = append(noWSTokens, token)
+		}
+	}
+	return noWSTokens
+}
+
+func process(line string) {
+	tokens := tokenize(line);
+	if len(tokens) == 0 {
+		fmt.Println("Nothing to evaluate");
+		return;
+	}
+	fmt.Printf("%q\n", tokens)
+}
 
 func main() {
 	prompt := "eclisp> "
@@ -15,11 +35,13 @@ func main() {
 		line := scanner.Text()
 		if len(line) > 0 {
 			scanner.AddToHistory(line)
-			fmt.Println(line)
+			process(line)
 		}
 	}
 
 	if err := scanner.Err(); err != nil {
 		panic(err)
+	} else {
+		fmt.Println("Goodbye!")
 	}
 }
