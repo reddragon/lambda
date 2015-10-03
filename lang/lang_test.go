7bf274bb4c1cb9b1975aa6df3446164f38cdd086
Gaurav Menghani <gauravm@fb.com>
2015-10-03 02:51:34 -0700

    Unbreak tests

--- lang/lang_test.go
+++ lang/lang_test.go
@@ -8,28 +8,28 @@ import (
 )
 
 func saneExprTest(query string, t *testing.T, env *LangEnv) {
-	val, _ := Eval(query, env)
-	if val.Val == nil || val.Err != nil {
-		t.Errorf("Expected %s to not be nil. Err: %s", query, val.Err)
+	val := Eval(query, env)
+	if len(val.ValStr) == 0 || len(val.ErrStr) > 0 {
+		t.Errorf("Expected %s to not be nil. Err: %s", query, val.ErrStr)
 	}
 }
 
 func checkExprResultTest(query, expected string, t *testing.T, env *LangEnv) {
-	val, _ := Eval(query, env)
-	if val.Val == nil || val.Err != nil {
-		t.Errorf("Expected %s to not be nil. Err: %s", query, val.Err)
+	val := Eval(query, env)
+	if len(val.ValStr) == 0 || len(val.ErrStr) > 0 {
+		t.Errorf("Expected %s to not be nil. Err: %s", query, val.ErrStr)
 	} else {
-		if val.Val.Str() != expected {
-			t.Errorf("Expected %s to be %s, but was %s", query, expected, val.Val.Str())
+		if val.ValStr != expected {
+			t.Errorf("Expected %s to be %s, but was %s", query, expected, val.ValStr)
 		}
 	}
 }
 
 func malformedExprTest(query string, t *testing.T, env *LangEnv) {
-	val, _ := Eval(query, env)
-	if val.Val != nil {
-		t.Errorf("Expected value of %s to be nil, was %s", val.Val.Str())
-	} else if val.Err == nil {
+	val := Eval(query, env)
+	if len(val.ValStr) != 0 {
+		t.Errorf("Expected value of %s to be nil, was %s", val.ValStr)
+	} else if len(val.ErrStr) == 0 {
 		t.Errorf("Expected %s to return a non-nil error")
 	}
 }
